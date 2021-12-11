b19c9cad88a7d54138c08060f1de9495cc1cdaf1
alisidaniel <alisidaniel@gmail.com>
2021-12-11 16:27:56 +0100

    pet crud with relation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,21 @@ import { AppService } from './app.service';
 import { PetsModule } from './pets/pets.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OwnersModule } from './owners/owners.module';
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     PetsModule,
+    OwnersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { OwnersService } from '../owners/owners.service';
+import { Repository } from 'typeorm';
+import { CreatePetInput } from './dto/create-pet.input';
 import { Pet } from './pet.entity';
+import { Owner } from 'src/owners/entities/owner.entity';
 
 @Injectable()
 export class PetsService {
-  async findAll(): Promise<Pet[]> {
-    const pet = new Pet();
-    pet.id = 1;
-    pet.name = 'Cat';
+  constructor(
+    @InjectRepository(Pet) private petRepository: Repository<Pet>,
+    private ownersService: OwnersService,
+  ) {}
 
-    return [pet];
+  createPet(createPetInput: CreatePetInput): Promise<Pet> {
+    const newPet = this.petRepository.create(createPetInput);
+    return this.petRepository.save(newPet);
+  }
+
+  async findAll(): Promise<Pet[]> {
+    return this.petRepository.find();
+  }
+
+  async findOne(id: number): Promise<Pet> {
+    return this.petRepository.findOneOrFail(id);
+  }
+
+  getOwner(ownerId: number): Promise<Owner> {
+    return this.ownersService.findOne(ownerId);
   }
 }
